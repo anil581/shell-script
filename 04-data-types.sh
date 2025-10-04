cb530360d50288a5f6a3c7ec4e51b019b5231af7
Anil kumar reddy <info@joindevops.com>
2025-10-04 23:05:14 +0530

    sum of two numbers

--- 04-data-types.sh
+++ 04-data-types.sh
@@ -5,4 +5,4 @@ Number2=$1
 timestamp=$(date)
 echo "script executed at:$timestamp
 sum=$(($Number1+$Number2))
-echo "sum of $Number1 and $Number2 :$sum"
+echo "sum of $Number1 and $Number2 is :$sum"
